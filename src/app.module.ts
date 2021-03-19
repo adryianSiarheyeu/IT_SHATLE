4215f2317ceaabe908388bb29f82e7a3d2911164
Adrik <1998adrian98@gmail.com>
2021-03-19 16:04:34 +0300

    auth & token modules added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,19 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import { UserModule } from './user/user.module';
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { UserModule } from "./user/user.module";
+import { AuthModule } from './auth/auth.module';
+import { TokenModule } from './token/token.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGODB_CONNECTION_STRING, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true,
+      useCreateIndex: true
     }),
     UserModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+    AuthModule,
+    TokenModule
+  ]
 })
 export class AppModule {}
